18a568f2f8489a9c256493439305c172e45effc5
Kabir Oberai <oberai.kabir@gmail.com>
2024-04-18 00:36:15 -0400

    wip

--- Sources/CWacroPluginRaw/section.c
+++ Sources/CWacroPluginRaw/section.c
@@ -1,8 +1,11 @@
-#include <stdint.h>
+#include "section.h"
 
 #define WACRO_ABI_VERSION_RAW 1
 
 #define _STR(X) #X
 #define STR(X) _STR(X)
+
+// this must be a metadata section rather than a data section so we can't
+// use __attribute__((section)) for it
 __asm__("\t.section .custom_section.wacro_abi,\"\",@\n\t.4byte " STR(WACRO_ABI_VERSION_RAW) "\n");
 const uint32_t WACRO_ABI_VERSION = WACRO_ABI_VERSION_RAW;
